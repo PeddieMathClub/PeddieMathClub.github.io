fa996aa46b0670b91ed54ec298c64f1a1e71bd6c
glolichen <jlche18659@gmail.com>
2024-09-09 21:30:26 -0400

    some math club stuff

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Peddie Math Club';
+export const SITE_DESCRIPTION = 'Welcome to the Peddie Math Club!';
